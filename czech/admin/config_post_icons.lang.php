aa7ca455718620c3a0629a8e434349c1e0ffbf48
Josef Schlehofer <pepe.schlehofer@gmail.com>
2018-11-03 17:40:28 +0100

    Ikona → Ikonka?

--- czech/admin/config_post_icons.lang.php
+++ czech/admin/config_post_icons.lang.php
@@ -6,20 +6,20 @@
  *
  * Issues and pull request belongs to the https://github.com/Eldenroot/MyBB_CZE-translation-1.8.x/
  */
-$l['post_icons'] = "Ikony příspěvků";
-$l['add_post_icon'] = "Přidat novou ikonu příspěvků";
-$l['add_post_icon_desc'] = "Zde můžete vložit novou ikonu příspěvku.";
+$l['post_icons'] = "Ikonky příspěvků";
+$l['add_post_icon'] = "Přidat novou ikonku příspěvků";
+$l['add_post_icon_desc'] = "Zde můžete vložit novou ikonku příspěvku.";
 $l['add_multiple_post_icons'] = "Přidat více ikon najednou";
 $l['add_multiple_post_icons_desc'] = "Zde můžete přidat více ikon pro nové příspěvky.";
 $l['edit_post_icon'] = "Změnit ikonu příspěvku";
-$l['edit_post_icon_desc'] = "Zde můžete změnit novou ikonu příspěvku.";
-$l['manage_post_icons'] = "Spravovat ikony příspěvku";
-$l['manage_post_icons_desc'] = "V této sekci můžete editovat, mazat a spravovat ikony příspěvků.";
+$l['edit_post_icon_desc'] = "Zde můžete změnit novou ikonku příspěvku.";
+$l['manage_post_icons'] = "Spravovat ikonky příspěvku";
+$l['manage_post_icons_desc'] = "V této sekci můžete editovat, mazat a spravovat ikonky příspěvků.";
 
-$l['name_desc'] = "Toto je název ikony příspěvku.";
-$l['image_path'] = "Cesta k ikoně";
-$l['image_path_desc'] = "Toto je cesta k ikoně příspěvku. Pokud chcete použít jinou ikonu příspěvku pro jinou šablonu, použijte prosím <strong>{theme}</strong> pro adresář obrázků v každé šabloně.";
-$l['save_post_icon'] = "Uložit ikonu příspěvku";
+$l['name_desc'] = "Toto je název ikonky příspěvku.";
+$l['image_path'] = "Cesta k ikonce";
+$l['image_path_desc'] = "Toto je cesta k ikonce příspěvku. Pokud chcete použít jinou ikonku příspěvku pro jinou šablonu, použijte prosím <strong>{theme}</strong> pro adresář obrázků v každé šabloně.";
+$l['save_post_icon'] = "Uložit ikonku příspěvku";
 $l['reset'] = "Reset";
 
 $l['path_to_images'] = "Cesta k obrázkům";
@@ -29,19 +29,19 @@ $l['image'] = "Ikonka";
 $l['add'] = "Přidat?";
 $l['save_post_icons'] = "Uložit ikonu příspěvku.";
 
-$l['no_post_icons'] = "Na fóru se momentálně nenachazí žádné ikony příspěvku.";
+$l['no_post_icons'] = "Na fóru se momentálně nenachazí žádné ikonky příspěvku.";
 
-$l['error_missing_name'] = "Nezadal(a) jste název ikony příspěvku";
-$l['error_missing_path'] = "Nezadal(a) jste cestu k ikoně příspěvku";
+$l['error_missing_name'] = "Nezadal(a) jste název ikonky příspěvku";
+$l['error_missing_path'] = "Nezadal(a) jste cestu k ikonce příspěvku";
 $l['error_missing_path_multiple'] = "Nezadal(a) jste cestu.";
 $l['error_invalid_path'] = "Nezadal(a) jste správnou cestu.";
-$l['error_no_images'] = "V adresáři se nenechází ikona příspěvku, nebo byly všechny ikony z adresáře již předány.";
-$l['error_none_included'] = "Nevybral(a) jste žádnou ikonu.";
-$l['error_invalid_post_icon'] = "Tato ikona zřejmě neexistuje.";
+$l['error_no_images'] = "V adresáři se nenechází ikonka příspěvku, nebo byly všechny ikonky z adresáře již předány.";
+$l['error_none_included'] = "Nevybral(a) jste žádnou ikonku.";
+$l['error_invalid_post_icon'] = "Tato ikonka zřejmě neexistuje.";
 
-$l['success_post_icon_added'] = "Ikona příspěvku byla úspěšně přidána.";
-$l['success_post_icons_added'] = "Ikony příspěvků byly úspěšně nahrány.";
-$l['success_post_icon_updated'] = "Ikona příspěvku byla úspěšně změněna.";
-$l['success_post_icon_deleted'] = "Zvolena ikona byla úspěšně smazána.";
+$l['success_post_icon_added'] = "Ikonka příspěvku byla úspěšně přidána.";
+$l['success_post_icons_added'] = "Ikonky příspěvků byly úspěšně nahrány.";
+$l['success_post_icon_updated'] = "Ikonka příspěvku byla úspěšně změněna.";
+$l['success_post_icon_deleted'] = "Zvolena ikonka byla úspěšně smazána.";
 
-$l['confirm_post_icon_deletion'] = "Opravdu si přejete smazat tuto ikonu?";
+$l['confirm_post_icon_deletion'] = "Opravdu si přejete smazat tuto ikonku?";
